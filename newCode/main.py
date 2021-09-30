9e2b9e4e20f2a1d562c6a4f07acbb27b08c0904c
cvinyard98 <cameronvin@gmail.com>
2021-09-30 17:07:54 -0600

    fixed gyman

--- newCode/main.py
+++ newCode/main.py
@@ -1,6 +1,6 @@
 from harger import greeting_josey
 import vinyardCode
-from Gyman import Gyman_def
+from Gyman import Gyman
 import mease
 
 
@@ -8,5 +8,5 @@ print("Welcome to our collaborative program.")
 
 greeting_josey()
 vinyardCode.cameron()
-Gyman_def()
+Gyman()
 mease.ian()
