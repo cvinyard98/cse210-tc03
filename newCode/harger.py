991e83c0a10a2c309ebea7609822f43a43ebdda4
W33bWhacker <91571842+W33bWhacker@users.noreply.github.com>
2021-09-30 16:57:50 -0600

    personal greeting

---
+++ newCode/harger.py
@@ -0,0 +1,4 @@
+
+def greeting_josey():
+
+    print("\nSup fags. We cool? ")
--- newCode/main.py
+++ newCode/main.py
@@ -1,1 +1,4 @@
+from newCode.harger import greeting_josey
+from harger import greeting_josey
+
 print("Welcome to our collaborative program.")
